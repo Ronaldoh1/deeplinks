725fbb70891a06bf027588cc8c6b9f9a57524cb0
Ronaldoh1 <2ronald.hernandez@gmail.com>
2020-03-14 13:12:08 -0400

    reset

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/apple-app-site-association' => 'home#apple_app_site_association'
 
-  root "todo_lists#index"
+  #root "todo_lists#index"
 
 end
