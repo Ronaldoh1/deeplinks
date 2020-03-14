549eb75bdf7136ac000c5925e200997a8421546e
Ronaldoh1 <2ronald.hernandez@gmail.com>
2020-03-14 14:42:33 -0400

    added new routes for aasa

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :todo_lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/.well-know/apple-app-site-association', :to => "home#apple_app_site"
+  get '/.well-know/apple-app-site-association', :to => "todo_lists#apple_app_site"
+  get '/apple-app-site-association', :to => "todo_lists#apple_app_site"
 
   
   root "todo_lists#index"
